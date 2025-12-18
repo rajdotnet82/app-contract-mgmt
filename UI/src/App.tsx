b5a9f99ee5e92fea17f3e5e2364b5dc63bc4ba0f
rajdotnet82 <rajdotnet82@gmail.com>
2025-12-18 15:41:52 -0600

    remove old signin

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -1,6 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
-import SignIn from "./pages/AuthPages/SignIn";
-import SignUp from "./pages/AuthPages/SignUp";
 import NotFound from "./pages/OtherPage/NotFound";
 import UserProfiles from "./pages/UserProfiles";
 import Videos from "./pages/UiElements/Videos";
@@ -91,10 +89,6 @@ export default function App() {
             </Route>
           </Route>
           {/* Dashboard Layout */}
-
-          {/* Auth Layout */}
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
 
           {/* Fallback Route */}
           <Route path="*" element={<NotFound />} />
